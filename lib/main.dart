38daf1eb0fca13f77beb4f10d3cf30063081f3dc
TSUYURI <tsuyuri@grit-group.co.jp>
2020-10-09 15:50:36 +0900

    complete

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
     Widget titleSection = Container(
       padding: const EdgeInsets.all(32),
       child: Row(
@@ -38,20 +40,74 @@ class MyApp extends StatelessWidget {
         ],
       ),
     );
+
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildButtonColumn(Colors.black, Icons.home, "ホーム"),
+          _buildButtonColumn(color, Icons.calendar_today, "ダイアリー"),
+          _buildButtonColumn(color, Icons.share, "Share"),
+        ],
+      ),
+    );
+
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+        //折り返し文字の設定
+        softWrap: true,
+      ),
+    );
+
     return MaterialApp(
       title: 'Flutter layout',
       home: Scaffold(
         appBar: AppBar(
           title: Text('Flutter layout'),
         ),
-        body: Column(
+        body: ListView(
+          //Column --> ListView
           children: [
+            Image.asset(
+              'images/lake.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
             titleSection,
+            buttonSection,
+            textSection,
           ],
         ),
       ),
     );
   }
 
-  Column _buildButtonColumn(Color color, IconData icon, String label) {}
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 }
