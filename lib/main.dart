55b2fb5698b18e158a72d41186597f31f6f587f2
TSUYURI <tsuyuri@grit-group.co.jp>
2020-10-09 18:07:22 +0900

    statefull実装 2クラス追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+// Uncomment lines 7 and 10 to view the visual layout at runtime.
+// import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
-void main() => runApp(MyApp());
+void main() {
+  // debugPaintSizeEnabled = true;
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Color color = Theme.of(context).primaryColor;
-
     Widget titleSection = Container(
       padding: const EdgeInsets.all(32),
       child: Row(
         children: [
           Expanded(
+            /*1*/
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                /*2*/
                 Container(
                   padding: const EdgeInsets.only(bottom: 8),
                   child: Text(
-                    'Obscene Lake Campground',
-                    style: TextStyle(fontWeight: FontWeight.bold),
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
                 Text(
-                  'kiersten, Switzerland',
+                  'Kandersteg, Switzerland',
                   style: TextStyle(
                     color: Colors.grey[500],
                   ),
@@ -32,22 +38,21 @@ class MyApp extends StatelessWidget {
               ],
             ),
           ),
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          Text('41'),
+          /*3*/
+          FavoriteWidget(),
         ],
       ),
     );
+
+    Color color = Theme.of(context).primaryColor;
 
     Widget buttonSection = Container(
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          _buildButtonColumn(Colors.black, Icons.home, "ホーム"),
-          _buildButtonColumn(color, Icons.calendar_today, "ダイアリー"),
-          _buildButtonColumn(color, Icons.share, "Share"),
+          _buildButtonColumn(color, Icons.call, 'CALL'),
+          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+          _buildButtonColumn(color, Icons.share, 'SHARE'),
         ],
       ),
     );
@@ -61,19 +66,17 @@ class MyApp extends StatelessWidget {
         'half-hour walk through pastures and pine forest, leads you to the '
         'lake, which warms to 20 degrees Celsius in the summer. Activities '
         'enjoyed here include rowing, and riding the summer toboggan run.',
-        //折り返し文字の設定
         softWrap: true,
       ),
     );
 
     return MaterialApp(
-      title: 'Flutter layout',
+      title: 'Flutter layout demo',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Flutter layout'),
+          title: Text('Flutter layout demo'),
         ),
         body: ListView(
-          //Column --> ListView
           children: [
             Image.asset(
               'images/lake.jpg',
@@ -105,6 +108,53 @@ class MyApp extends StatelessWidget {
               fontWeight: FontWeight.w400,
               color: color,
             ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = false;
+  int _favoriteCount = 0;
+// ···
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            padding: EdgeInsets.all(0),
+            alignment: Alignment.centerRight,
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoriteCount'),
           ),
         ),
       ],
