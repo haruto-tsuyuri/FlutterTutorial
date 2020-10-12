44e607a1b388271ec8e171dc42d106a4ffc581f6
TSUYURI <tsuyuri@grit-group.co.jp>
2020-10-12 12:24:24 +0900

    change

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 // Uncomment lines 7 and 10 to view the visual layout at runtime.
 // import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
 void main() {
   // debugPaintSizeEnabled = true;
   runApp(MyApp());
+}
+
+class TapboxA extends StatefulWidget {
+  TapboxA({Key key}) : super(key: key);
+
+  @override
+  _TapboxAState createState() => _TapboxAState();
+}
+
+class _TapboxAState extends State<TapboxA> {
+  bool _active = false;
+
+  void _handleTap() {
+    setState(() {
+      _active = !_active;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            _active ? 'Active' : 'Inactive',
+            style: TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+          color: _active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
+      ),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -23,14 +60,14 @@ class MyApp extends StatelessWidget {
                 Container(
                   padding: const EdgeInsets.only(bottom: 8),
                   child: Text(
-                    'Oeschinen Lake Campground',
+                    'B/Y',
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
                 Text(
-                  'Kandersteg, Switzerland',
+                  '東京都品川区五反田',
                   style: TextStyle(
                     color: Colors.grey[500],
                   ),
@@ -71,22 +108,33 @@ class MyApp extends StatelessWidget {
     );
 
     return MaterialApp(
-      title: 'Flutter layout demo',
+      title: 'Contact Us',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Flutter layout demo'),
+          shape: ContinuousRectangleBorder(
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(30),
+                  bottomRight: Radius.circular(30))),
+          centerTitle: true,
+          title: Text(
+            'Contact Us',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          backgroundColor: Colors.black54,
+          // shadowColor: Colors.black,
         ),
         body: ListView(
           children: [
             Image.asset(
-              'images/lake.jpg',
-              width: 600,
-              height: 240,
+              'images/logoB.png',
               fit: BoxFit.cover,
             ),
             titleSection,
             buttonSection,
             textSection,
+            TapboxA(),
           ],
         ),
       ),
@@ -167,3 +215,120 @@ class _FavoriteWidgetState extends State<FavoriteWidget> {
     );
   }
 }
+
+// class MyApp extends StatelessWidget {
+//   static const String _title = 'Flutter Code Sample';
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: _title,
+//       home: HomePage(),
+//     );
+//   }
+// }
+//
+// class HomePage extends StatefulWidget {
+//   @override
+//   _HomePageState createState() => _HomePageState();
+// }
+//
+// class _HomePageState extends State<HomePage> {
+//   int _selectedIndex = 0;
+//   PageController _pageController;
+//
+//   static List<Widget> _pageList = [
+//     CustomPage(pannelColor: Colors.cyan, title: 'Home'),
+//     CustomPage(pannelColor: Colors.green, title: 'Settings'),
+//     CustomPage(pannelColor: Colors.pink, title: 'Search')
+//   ];
+//
+//   void _onPageChanged(int index) {
+//     setState(() {
+//       _selectedIndex = index;
+//     });
+//   }
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     _pageController = PageController(
+//       initialPage: _selectedIndex,
+//     );
+//   }
+//
+//   @override
+//   void dispose() {
+//     _pageController.dispose();
+//     super.dispose();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('サンプル2'),
+//       ),
+//       body: PageView(
+//         controller: _pageController,
+//         onPageChanged: _onPageChanged,
+//         children: _pageList,
+//       ),
+//       bottomNavigationBar: BottomNavigationBar(
+//         items: const <BottomNavigationBarItem>[
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.home),
+//             title: Text('Home'),
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.settings),
+//             title: Text('Setting'),
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.search),
+//             title: Text('Search'),
+//           ),
+//         ],
+//         currentIndex: _selectedIndex,
+//         onTap: (index) {
+//           _selectedIndex = index;
+//
+//           _pageController.animateToPage(index,
+//               duration: Duration(milliseconds: 300), curve: Curves.easeIn);
+//         },
+//       ),
+//     );
+//   }
+// }
+
+// class CustomPage extends StatelessWidget {
+//   final Color pannelColor;
+//   final String title;
+//
+//   CustomPage({@required this.pannelColor, @required this.title});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     final titleTextStyle = Theme.of(context).textTheme.title;
+//     return Container(
+//       child: Center(
+//         child: Container(
+//           width: 200,
+//           height: 200,
+//           decoration: BoxDecoration(
+//               color: pannelColor,
+//               borderRadius: BorderRadius.all(Radius.circular(20.0))),
+//           child: Center(
+//             child: Text(
+//               title,
+//               style: TextStyle(
+//                 fontSize: titleTextStyle.fontSize,
+//                 color: titleTextStyle.color,
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
