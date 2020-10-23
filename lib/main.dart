2833ae33cae6bb9fd0bd3f17192fae29f10daec3
TSUYURI <tsuyuri@grit-group.co.jp>
2020-10-23 12:24:05 +0900

    test_ナビ_ページングなし

--- lib/main.dart
+++ lib/main.dart
@@ -1,116 +1,296 @@
 import 'package:flutter/material.dart';
 
-import 'bottom_bar_fab_flutter.dart';
-import 'fab_with_icons.dart';
-import 'layout.dart';
-
 // Uncomment lines 7 and 10 to view the visual layout at runtime.
 // import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
-void main() => runApp(new MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  static const String _title = 'Flutter code';
+
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'BottomAppBar with FAB'),
+    return MaterialApp(
+      title: _title,
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
+class HomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
-  String _lastSelected = '画面 :0';
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  static List<Widget> _pageList = [
+    CustomPage(
+        pannelColor: Colors.blueAccent, title: 'Home', icon: Icon(Icons.home)),
+    CustomPage(
+        pannelColor: Colors.blueAccent,
+        title: 'Dialog',
+        icon: Icon(Icons.class_)),
+    CustomPage(
+        pannelColor: Colors.blueAccent,
+        title: 'Setting',
+        icon: Icon(Icons.settings)),
+  ];
 
-  void _selectedTab(int index) {
-    int screenNum = index + 1;
+  void _onItemTapped(int index) {
     setState(() {
-      _lastSelected = '画面 : $screenNum';
-    });
-  }
-
-  void _selectedFab(int index) {
-    int screenNum = index + 1;
-    setState(() {
-      _lastSelected = 'FAB: $screenNum';
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('navi'),
       ),
-      body: Center(
-        child: Text(
-          _lastSelected,
-          style: TextStyle(fontSize: 32.0),
-        ),
-      ),
-      bottomNavigationBar: FABBottomAppBar(
-        centerItemText: 'A',
-        color: Colors.grey,
-        selectedColor: Colors.blueAccent,
-        notchedShape: CircularNotchedRectangle(),
-        onTabSelected: _selectedTab,
-        items: [
-          FABBottomAppBarItem(iconData: Icons.home, text: 'ホーム'),
-          FABBottomAppBarItem(iconData: Icons.class_, text: 'ダイアログ'),
-          FABBottomAppBarItem(iconData: Icons.dashboard, text: 'Bottom'),
-          FABBottomAppBarItem(
-              iconData: Icons.settings_applications, text: '設定'),
-        ],
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: _buildFab(
-          context), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-
-  Widget _buildFab(BuildContext context) {
-    final icons = [Icons.sms, Icons.mail, Icons.phone];
-    return AnchoredOverlay(
-      showOverlay: true,
-      overlayBuilder: (context, offset) {
-        return CenterAbout(
-          position: Offset(offset.dx, offset.dy - icons.length * 35.0),
-          child: FabWithIcons(
-            icons: icons,
-            onIconTapped: _selectedFab,
+      body: _pageList[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
           ),
-        );
-      },
-      child: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-        elevation: 2.0,
+          BottomNavigationBarItem(
+            icon: Icon(Icons.class_),
+            title: Text('Dialog'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            title: Text('設定'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.orangeAccent,
+        // fixedColor: Colors.blueAccent,
+        selectedItemColor: Colors.lightBlueAccent,
+        unselectedItemColor: Colors.white,
       ),
     );
   }
 }
+
+class CustomPage extends StatelessWidget {
+  final Color pannelColor;
+  final String title;
+  final Icon icon;
+
+  CustomPage(
+      {@required this.pannelColor, @required this.title, @required this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    final titleTextStyle = Theme.of(context).textTheme.title;
+    return Container(
+      child: Center(
+        child: Container(
+          width: 200,
+          height: 200,
+          decoration: BoxDecoration(
+              color: pannelColor,
+              borderRadius: BorderRadius.all(Radius.circular(29.0))),
+          child: Center(
+            child: Text(
+              title,
+              style: TextStyle(
+                fontSize: titleTextStyle.fontSize,
+                color: titleTextStyle.color,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+// class MainPage extends StatefulWidget {
+//   @override
+//   _MainPageState createState() => _MainPageState();
+// }
+//
+// class _MainPageState extends State<MainPage> {
+//   int _currentIndex = 0;
+//   final _pageWidgets = [
+//     PageWidget(color: Colors.white, title: 'Home'),
+//     PageWidget(color: Colors.blueGrey, title: 'Home'),
+//     PageWidget(color: Colors.orangeAccent, title: 'Home'),
+//   ];
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('BTNG'),
+//       ),
+//       body: _pageWidgets.elementAt(_currentIndex),
+//       bottomNavigationBar: BottomNavigationBar(
+//         items: <BottomNavigationBarItem>[
+//           BottomNavigationBarItem(
+//               icon: Icon(Icons.home), title: Text(('Home'))),
+//           BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Album')),
+//           BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Chat')),
+//         ],
+//         currentIndex: _currentIndex,
+//         fixedColor: Colors.blueAccent,
+//         onTap: _onItemTapped,
+//         type: BottomNavigationBarType.fixed,
+//       ),
+//     );
+//   }
+//
+//   void _onItemTapped(int index) => setState(() => _currentIndex = index);
+// }
+
+// class PageWidget extends StatefulWidget {
+//   @override
+//   _PageWidget createState() => _PageWidget();
+// }
+//
+// class _PageWidget extends State<PageWidget> {
+//
+//   final Color color;
+//   final String tilte;
+//
+//   // PageWidget({Key key, this.color, this.title}) : super(key: key);
+//
+// }
+
+// class PageWidget extends StatelessWidget {
+//   final Color color;
+//   final String title;
+//
+//   PageWidget({Key key, this.color, this.title}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       color: color,
+//       child: Center(
+//         child: Text(
+//           title,
+//           style: TextStyle(
+//             fontSize: 25,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// samp2
+// class MyApp extends StatelessWidget {
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return new MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: new ThemeData(
+//         // This is the theme of your application.
+//         //
+//         // Try running your application with "flutter run". You'll see the
+//         // application has a blue toolbar. Then, without quitting the app, try
+//         // changing the primarySwatch below to Colors.green and then invoke
+//         // "hot reload" (press "r" in the console where you ran "flutter run",
+//         // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
+//         // counter didn't reset back to zero; the application is not restarted.
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: new MyHomePage(title: 'BottomAppBar with FAB'),
+//     );
+//   }
+// }
+//
+// class MyHomePage extends StatefulWidget {
+//   MyHomePage({Key key, this.title}) : super(key: key);
+//
+//   final String title;
+//
+//   @override
+//   _MyHomePageState createState() => new _MyHomePageState();
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+//   String _lastSelected = '画面 :0';
+//
+//   void _selectedTab(int index) {
+//     int screenNum = index + 1;
+//     setState(() {
+//       _lastSelected = '画面 : $screenNum';
+//     });
+//   }
+//
+//   void _selectedFab(int index) {
+//     int screenNum = index + 1;
+//     setState(() {
+//       _lastSelected = 'FAB: $screenNum';
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Text(
+//           _lastSelected,
+//           style: TextStyle(fontSize: 32.0),
+//         ),
+//       ),
+//       bottomNavigationBar: FABBottomAppBar(
+//         centerItemText: 'A',
+//         color: Colors.grey,
+//         selectedColor: Colors.blueAccent,
+//         notchedShape: CircularNotchedRectangle(),
+//         onTabSelected: _selectedTab,
+//         items: [
+//           FABBottomAppBarItem(iconData: Icons.home, text: 'ホーム'),
+//           FABBottomAppBarItem(iconData: Icons.class_, text: 'ダイアログ'),
+//           FABBottomAppBarItem(iconData: Icons.dashboard, text: 'Bottom'),
+//           FABBottomAppBarItem(
+//               iconData: Icons.settings_applications, text: '設定'),
+//         ],
+//       ),
+//       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+//       floatingActionButton: _buildFab(
+//           context), // This trailing comma makes auto-formatting nicer for build methods.
+//     );
+//   }
+//
+//   Widget _buildFab(BuildContext context) {
+//     final icons = [Icons.sms, Icons.mail, Icons.phone];
+//     return AnchoredOverlay(
+//       showOverlay: true,
+//       overlayBuilder: (context, offset) {
+//         return CenterAbout(
+//           position: Offset(offset.dx, offset.dy - icons.length * 35.0),
+//           child: FabWithIcons(
+//             icons: icons,
+//             onIconTapped: _selectedFab,
+//           ),
+//         );
+//       },
+//       child: FloatingActionButton(
+//         onPressed: () {},
+//         tooltip: 'Increment',
+//         child: Icon(Icons.add),
+//         elevation: 2.0,
+//       ),
+//     );
+//   }
+// }
+
+// samp1
 // class MyApp extends StatelessWidget {
 //   static const String _title = 'Flutter Code Sample';
 //
